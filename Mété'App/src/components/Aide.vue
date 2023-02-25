64da680bf2b1995049aa91ea5ecb6ac58ab36e4a
Erwan LE ROUX <102350313+RPZDesMicroOndes@users.noreply.github.com>
2023-02-25 16:52:44 +0100

    Ajout Page d'aide

---
+++ Mété'App/src/components/Aide.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Page d'aide</h1>
+
+        <p>
+            Cette page vous explique comment fonctionne l'application `Mété' App`.<br>
+            Vous pouvez rechercher la météo d'une ville en tapant son nom dans la barre de recherche.<br>
+            Ensuite, appuyez sur le bouton `Rechercher` pour afficher la météo de la ville sélectionnée.<br>
+            Les informations affichées permettent de connaître la météo de la ville sélectionnée pour les 5 prochains jours.<br>
+        </p>
+
+        <p>
+            Pour tout autre informations et requêtes, vous pouvez nous contacter via le formulaire de la page `Contact`.<br>
+        </p>
+    </div>
+</template>
