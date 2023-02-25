d28e21bf941a193536d7b06a7d9b87e4eadb0d12
Evah <eve-anneoffredi@orange.fr>
2023-02-25 19:41:56 +0100

    end

--- Mété'App/src/components/NavBar.vue
+++ Mété'App/src/components/NavBar.vue
@@ -1,56 +1,54 @@
 <template>
 
-    <nav>
-        <img
-            alt="Vue logo"
-            class="logo"
-            src="../assets/logo.png"
-            width="80"
-            height="80"
-        />
+<nav>
+    <img
+        alt="Vue logo"
+        class="logo"
+        src="../assets/logo.png"
+        width="80"
+        height="80"
+    />
 
-        <div class="btnSpace">
-            <div class="btn">
-                <router-link class="btn-txt" to="/">Accueil</router-link>
-            </div>
-            <div class="btn">
-                <router-link class="btn-txt" to="/contact">Contact</router-link>
-            </div>
-            <div class="btn">
-                <router-link class="btn-txt" to="/help">Aide</router-link>
-            </div>
+    <div class="btnSpace">
+        <div class="btn">
+            <router-link class="btn-txt" to="/">Accueil</router-link>
         </div>
-    </nav>
-  </template>
+        <div class="btn">
+            <router-link class="btn-txt" to="/contact">Contact</router-link>
+        </div>
+        <div class="btn">
+            <router-link class="btn-txt" to="/help">Aide</router-link>
+        </div>
+    </div>
+</nav>
+</template>
   
-  <style>
-    .nav-link {
-        display: inline-block;
-        padding: 0.5em 1em;
-        color: white;
-        text-decoration: none;
-    }
+<style>
+.nav-link {
+    display: inline-block;
+    padding: 0.5em 1em;
+    color: white;
+    text-decoration: none;
+}
 
-    .btnSpace{
-        display : flex;
-        gap : 15px;
-    }
+.btnSpace{
+    display : flex;
+    gap : 15px;
+}
 
-    .btn{
-        background-color: #CF8E80;;
-        border-radius: 5px;
-        padding : 5px 15px 5px 15px;
-        cursor: pointer;
-    }
+.btn{
+    background-color: #CF8E80;;
+    border-radius: 5px;
+    padding : 5px 15px 5px 15px;
+    cursor: pointer;
+}
 
-    .btn:hover {
-        background-color: #c68a7d;
-    }
+.btn:hover {
+    background-color: #c68a7d;
+}
 
-    .btn-txt{
-        color : white;
-    }
-
-
-  </style>
+.btn-txt{
+    color : white;
+}
+</style>
   
